8916244affffb7d08ef004592f9e19e5ba3d5308
machinegirl <machinegirl@eternalvoid.net>
2019-03-01 23:45:16 -0500

    menu, workshops, changes to index, client-r, services

--- src/modules.js
+++ src/modules.js
@@ -9,27 +9,40 @@
   
   // Add the HTML snippet to the DOM.
   .then(res => {
-    const header = document.getElementById('header_space');
+    const header = document.getElementById('header-space');
     if (header) {
       header.innerHTML = res;
-    }
+      insertMenu('header-menu-space');
+      }
   });
 
   fetch('footer.html')
   .then(res1 => res1.text())
   .then(res1 => {
-    const footer = document.getElementById('footer_space');
+    const footer = document.getElementById('footer-space');
     if (footer) {
       footer.innerHTML = res1;
+      insertMenu('footer-menu-space');
     }
   });
 
   fetch('right-col.html')
   .then(res2 => res2.text())
   .then(res2 => {
-    const rightCol = document.getElementById('right-col_space');
+    const rightCol = document.getElementById('right-col-space');
     if (rightCol) {
       rightCol.innerHTML = res2;
     }
   });
 })();
+
+function insertMenu(id) {
+  fetch('menu.html')
+  .then(res3 => res3.text())
+  .then(res3 => {
+    const menu = document.getElementById(id);
+    if (menu) {
+      menu.innerHTML = res3;
+    }
+  });
+}
